15ad552bf5d9a5a4d1e65587dcded18d03a3b5c7
Wilson S. Goltara <wilson.goltara15@gmail.com>
2022-09-09 14:36:17 -0300

    initial commit

--- src/server.js
+++ src/server.js
@@ -4,5 +4,5 @@ require('dotenv').config();
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
 app.listen(process.env.PORT, () => {
-  console.log(`Escutando na porta ${process.env.PORT}`);
+  console.log(`Listening on port: ${process.env.PORT}`);
 });
