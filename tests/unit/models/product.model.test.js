84e168facd31074b32fa1563d2940024c88e7b22
Wilson S. Goltara <wilson.goltara15@gmail.com>
2022-09-15 12:56:59 -0300

    refactor: code formatter

--- tests/unit/models/product.model.test.js
+++ tests/unit/models/product.model.test.js
@@ -3,7 +3,11 @@ const sinon = require('sinon');
 
 const connection = require('../../../src/models/database/connection');
 const productsModel = require('../../../src/models/products.model');
-const { productsMock, productByIdMock, productNotFoundMock } = require('../../mocks/products.mocks');
+const {
+  productsMock,
+  productByIdMock,
+  productNotFoundMock,
+} = require('../../mocks/products.mocks');
 
 describe('Unit test the model layer of the application:', () => {
   afterEach(sinon.restore);
@@ -17,19 +21,18 @@ describe('Unit test the model layer of the application:', () => {
   });
 
   it("Test if it receives a products from the endpoint '/products/:id.", async () => {
-    sinon.stub(connection, "execute").resolves([productByIdMock]);
+    sinon.stub(connection, 'execute').resolves([productByIdMock]);
 
     const product = await productsModel.listProductById(1);
 
     expect(product).to.equal(productByIdMock);
   });
 
-  it("Test if it receives a error message from the endpoint '/products/:id' invalid.",
-    async () => {
-      sinon.stub(connection, "execute").resolves([productNotFoundMock]);
+  it("Test if it receives a error message from the endpoint '/products/:id' invalid.", async () => {
+    sinon.stub(connection, 'execute').resolves([productNotFoundMock]);
 
-      const productNotFound = await productsModel.listProductById(1000);
+    const productNotFound = await productsModel.listProductById(1000);
 
-      expect(productNotFound).to.equal(productNotFoundMock);
+    expect(productNotFound).to.equal(productNotFoundMock);
   });
 });
