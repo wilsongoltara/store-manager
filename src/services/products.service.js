7d2b17194e813e347192a5c62aac0de618eafaaf
Wilson S. Goltara <wilson.goltara15@gmail.com>
2022-09-14 22:22:36 -0300

    feat: add products service

---
+++ src/services/products.service.js
@@ -0,0 +1,22 @@
+const productsModel = require('../models/products.model');
+const { HTTP_NOT_FOUND, HTTP_OK_SUCCESS } = require('../utils/customMessage');
+
+const listProducts = async () => {
+  const products = await productsModel.listProducts();
+  return products;
+};
+
+const listProductById = async (productId) => {
+  const product = await productsModel.listProductById(productId);
+  
+  if (product && product.length === 0) {
+    return { statusCode: HTTP_NOT_FOUND, result: 'product not fount' };
+  }
+
+  if (product.length > 0) return { statusCode: HTTP_OK_SUCCESS, result: product };
+};
+
+module.exports = {
+  listProducts,
+  listProductById,
+};
---
+++ src/utils/customMessage.js
@@ -0,0 +1,7 @@
+const HTTP_OK_SUCCESS = 200;
+const HTTP_NOT_FOUND = 404;
+
+module.exports = {
+  HTTP_OK_SUCCESS,
+  HTTP_NOT_FOUND,
+};
