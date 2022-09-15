84f1e5ec8d32edfbacb7ee7c19f0a7970bec6cb2
Wilson S. Goltara <wilson.goltara15@gmail.com>
2022-09-14 22:23:16 -0300

    feat: add products controller

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,18 @@
+const productsService = require('../services/products.service');
+const { HTTP_OK_SUCCESS } = require('../utils/customMessage');
+
+const listProducts = async (_req, res) => {
+  const products = await productsService.listProducts();
+  res.status(HTTP_OK_SUCCESS).json(products);
+};
+
+const listProductById = async (req, res) => {
+  const { params: { id } } = req;
+  const { statusCode, result } = await productsService.listProductById(id);
+  res.status(statusCode).json(result);
+};
+
+module.exports = {
+  listProducts,
+  listProductById,
+};
