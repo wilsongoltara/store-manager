3c59f71c7d559dc48a0feae9860a48f59ebbe2f8
Wilson S. Goltara <wilson.goltara15@gmail.com>
2022-09-16 13:59:38 -0300

    refactor: add function next

--- src/middlewares/productsValidation.js
+++ src/middlewares/productsValidation.js
@@ -1,13 +1,16 @@
 const { HTTP_BAD_REQUEST, HTTP_UNPROCESSABLE_ENTITY } = require('../utils/customMessage');
 
-const validationNameProduct = (req, res) => {
+const validationNameProduct = (req, res, next) => {
   const { body: { name } } = req;
+
   if (!name) res.status(HTTP_BAD_REQUEST).json({ message: '"name" is required' });
   if (name.length < 5) {
     res
       .status(HTTP_UNPROCESSABLE_ENTITY)
       .json({ message: '"name" length must be at least 5 characters long' });
   }
+
+  next();
 };
 
 module.exports = { validationNameProduct };
